6dc3891597c6343f7ea1d3f792b14fd315366ed9
nthanmnda <nthanmnda@gmail.com>
2026-08-20 15:45:37 +0700

    coba

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UtamaController;
-//hola
+//ini adalah perubahan dari daniel
 // ini adalah homepage yang mengarah pada controller
 Route::get('/', [UtamaController::class, 'boleh']);
 
