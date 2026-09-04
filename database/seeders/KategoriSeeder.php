764881b9425177a78829d5554df84efced81ea28
Vella-24 <s161124009@student.ubaya.ac.id>
2026-09-04 14:51:06 +0700

    Menambahkan seeder kategori

---
+++ database/seeders/KategoriSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB; 
+
+class KategoriSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $dataku = [
+            ['nama' => 'Makanan Berat', 'deskripsi' => 'Ini adalah makanan yang akan memberatkan perutmu'],
+            ['nama' => 'Makanan Ringan', 'deskripsi' => 'Ini adalah makanan yang akan meringankan hidupmu'],
+            ['nama' => 'Jus', 'deskripsi' => 'Ini adalah minuman yang berasal dari olahan buah'],
+            ['nama' => 'Soda', 'deskripsi' => 'Ini adalah minuman yang membuatmu bersendawa'],
+            ['nama' => 'Susu', 'deskripsi' => 'Ini adalah minuman yang menyehatkan']
+        ];
+        DB::table('kategoris')->insert($dataku);
+    }
+}
