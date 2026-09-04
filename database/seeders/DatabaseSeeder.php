0cc4cad22ae8853ca80950c39455950a0d70f4a0
nthanmnda <nthanmnda@gmail.com>
2026-09-04 16:27:03 +0700

    Edit stok sama use db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call([BarangSeeder::Class]);
+        $this->call([BarangSeeder::class]);
     }
 }
